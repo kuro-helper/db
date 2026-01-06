84fbefb2ea403ae4cbdbbee1861fd736abaa1746
SeaotterMS <peter910820@gmail.com>
2026-01-06 22:25:17 +0800

    chore: tidy code

--- user.go
+++ user.go
@@ -17,3 +17,27 @@ func EnsureUserTx(tx *gorm.DB, userID, userName string) (*User, error) {
 	}
 	return &user, nil
 }
+
+// 取得指定使用者資料
+func GetUser(userID string) (User, error) {
+	var user User
+
+	err := Dbs.First(&user, "id = ?", userID).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+// 取得所有使用者資料
+func GetAllUser() ([]User, error) {
+	var user []User
+
+	err := Dbs.Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
