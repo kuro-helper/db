ba7e27a376af0dfcfb833d60ebdfa9b463318b1b
SeaotterMS <peter910820@gmail.com>
2026-01-07 20:18:16 +0800

    feat: adjust users table func

--- user.go
+++ user.go
@@ -18,7 +18,7 @@ func EnsureUserTx(tx *gorm.DB, userID, userName string) (*User, error) {
 	return &user, nil
 }
 
-// 取得指定使用者資料
+// 依據 userID 取得單一使用者資料
 func GetUser(userID string) (User, error) {
 	var user User
 
@@ -30,11 +30,35 @@ func GetUser(userID string) (User, error) {
 	return user, nil
 }
 
+// 與 GetUser 相同，但透過呼叫端提供的交易 tx 進行查詢
+func GetUserTx(tx *gorm.DB, userID string) (User, error) {
+	var user User
+
+	err := tx.First(&user, "id = ?", userID).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
 // 取得所有使用者資料
-func GetAllUser() ([]User, error) {
+func GetUsers() ([]User, error) {
 	var user []User
 
 	err := Dbs.Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+// 與 GetUsers 相同，但透過呼叫端提供的交易 tx 進行查詢
+func GetUsersTx(tx *gorm.DB) ([]User, error) {
+	var user []User
+
+	err := tx.Find(&user).Error
 	if err != nil {
 		return user, err
 	}
